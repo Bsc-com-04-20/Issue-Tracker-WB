21c958dfbdb2258de624b8b3fe1f7ac3bd6bf904
Brian Gomez <131519596+Bsc-com-04-20@users.noreply.github.com>
2026-05-28 23:50:43 +0200

    Delete Backend directory

--- Backend/Src/auth/dto/refresh-token.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength } from 'class-validator';
-
-export class RefreshTokenBodyDto {
-  @ApiProperty({ description: 'Opaque refresh token from login' })
-  @IsString()
-  @MinLength(10)
-  refreshToken: string;
-}
